eb286de2975fe2aa1a68a21e17a9fe606a0c424a
Minh Pham <mnphamx1@gmail.com>
2019-01-19 18:24:58 -0500

    Add stuff

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,4 +1,5 @@
 class ReceiptsController < ApplicationController
+    acts_as_token_authentication_handler_for User
     before_action :authenticate_user!, except: [:index]
     skip_before_action :verify_authenticity_token
 
@@ -12,9 +13,17 @@ class ReceiptsController < ApplicationController
     def createReceipt() 
         name = params[:name]
         description = params[:description]
+        user_id = current_user.id
         group_id = params[:groupID]
+        # line_items = params[:lineItems]
+
+        Receipt.new(:name => name, :description => description, :user_id => user_id, :group_id => group_id)
+
+        # for item in line_items
+        #     LineItems.new(:item => item.item, :price => item.price, :receipt =>item.receipt.id).save
         puts("Name: ", name)
         puts("Desc: ", description)
+        puts("UserID: ", user_id)
         puts("GroupID: ", group_id)
         render plain: ""
     end 
