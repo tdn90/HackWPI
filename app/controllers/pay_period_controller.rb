858cffbc0113dac3f9f77f76ff57e5f26606ef88
Minh Pham <mnphamx1@gmail.com>
2019-01-20 05:24:22 -0500

    Add create_pay_period

--- app/controllers/pay_period_controller.rb
+++ app/controllers/pay_period_controller.rb
@@ -4,8 +4,20 @@ class PayPeriodController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create_pay_period()
-        puts(Time.now.getlocal.zone)
+        groupID = params[:group_id]
+        current_time = Time.now
+        cur_year = current_time.year
+        cur_month = current_time.month
+        cur_day = current_time.day
+        startday = (cur_year.to_s + "-" + cur_month.to_s + "-" + cur_day.to_s).to_datetime
+        endday = startday
+        if(cur_day < 25)
+            endday = (startday >> 1) - cur_day
+        else
+            endday = (startday >> 2) - cur_day
+        end
 
+        Payperiod.create(start: startday, end: endday, archived: 1, group_id: groupID).save!
         render plain: ""
     end
 
